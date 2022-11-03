bc0926e34c1f6f1738a2f4471db604516318a3ae
darwin808 <darwin.apolinario12@gmail.com>
2022-11-03 17:28:43 +0800

    feat(tailwind): init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 // import styles from "../styles/Home.module.css"
 
 export default function Home(): any {
-   return <div>darwin</div>
+   return <h1 className="text-3xl font-bold underline text-red-100">Hello world!</h1>
 }
